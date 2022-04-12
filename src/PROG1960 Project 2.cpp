cdc03fe0a654d8c40602a7aadf68e890bcff268c
Joshua Rice <Jrice1041@conestogac.on.ca>
2022-04-12 04:50:23 -0400

    Start of New Menu

--- src/PROG1960 Project 2.cpp
+++ src/PROG1960 Project 2.cpp
@@ -14,4 +14,21 @@ using namespace std;
 int main() {
     std::cout << "Class Grouping Project v0.0.1!\n";
 
+    std::string paramFile;
+    std::string pairFile;
+
+    cin >> paramFile;
+    cin >> pairFile;
+    CSV pairCSV(pairFile);
+
+    Pairings pairObj(pairCSV);
+    Parameters paramObj(paramFile);
+
+    bool menu = true;
+    while(menu = true){
+
+
+    }
+
+
 }
